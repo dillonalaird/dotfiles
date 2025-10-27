4f88140ccdca58053170a21da30662fdafa54b72
Dillon Laird <dillonalaird@gmail.com>
2025-10-27 11:24:56 -0700

    add install for neovim

--- vastai/provisioning_script.sh
+++ vastai/provisioning_script.sh
@@ -7,6 +7,9 @@ set -eo pipefail
 # Activate the main virtual environment
 . /venv/main/bin/activate
 
+sudo apt update
+sudo apt install neovim
+
 git clone https://github.com/dillonalaird/dotfiles.git
 mkdir -p ~/.config/
 cp -r dotfiles/nvim/.config/nvim ~/.config/
