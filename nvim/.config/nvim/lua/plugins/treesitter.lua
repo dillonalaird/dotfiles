0f1255ad4b6e8fda03eb029aadeedd8ac951cebd
Dillon Laird <dillonalaird@gmail.com>
2025-11-23 15:18:43 -0800

    make diagnostics transparent

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,29 +5,40 @@ return {
 	dependencies = {
 		"sainnhe/gruvbox-material",
 
-		-- "sainnhe/everforest",
-		-- "ellisonleao/gruvbox.nvim",
-		-- "folke/tokyonight.nvim",
-		-- "rebelot/kanagawa.nvim",
+		-- lazy = false,
+		-- config = function()
+		-- 	vim.g.gruvbox_material_background = "medium"
+		-- 	vim.g.gruvbox_material_transparent_background = 1
+		-- 	-- makes the diagnostic popup transparent
+		-- 	-- this needs to be set before setting the colorscheme
+		-- 	vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=NONE]])
+		-- 	vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guibg=NONE]])
+		-- 	vim.cmd([[colorscheme gruvbox-material]])
+		-- end,
 
 		lazy = false,
 		config = function()
 			vim.g.gruvbox_material_background = "medium"
 			vim.g.gruvbox_material_transparent_background = 1
-			-- makes the diagnostic popup transparent
-			-- this needs to be set before setting the colorscheme
-			vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=NONE]])
-			vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guibg=NONE]])
+
+			vim.api.nvim_create_augroup("TransparentFloat", { clear = true })
+			vim.api.nvim_create_autocmd("ColorScheme", {
+				group = "TransparentFloat",
+				callback = function()
+					-- transparent backgrounds for common float containers
+					for _, g in ipairs({ "NormalFloat", "FloatBorder", "FloatTitle" }) do
+						vim.api.nvim_set_hl(0, g, { bg = "none" })
+					end
+
+					-- keep diagnostic foreground colors by linking float groups
+					local severities = { "Error", "Warn", "Info", "Hint" }
+					for _, s in ipairs(severities) do
+						vim.api.nvim_set_hl(0, "DiagnosticFloating" .. s, { link = "Diagnostic" .. s })
+					end
+				end,
+			})
 			vim.cmd([[colorscheme gruvbox-material]])
 		end,
-
-		-- lazy = false,
-		-- config = function()
-		-- vim.g.everforest_background = "medium"
-		-- 	vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=NONE]])
-		-- 	vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guibg=NONE]])
-		-- 	vim.cmd([[colorscheme everforest]])
-		-- end,
 	},
 
 	config = function()
